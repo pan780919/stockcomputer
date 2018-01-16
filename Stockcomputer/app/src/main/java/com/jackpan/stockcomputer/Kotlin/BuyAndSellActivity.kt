1223477cdf7f599f6dc1dc96b40b222aa1635bc1
JackPan <jheng.wei0919@gmail.com>
2018-01-16 18:54:56 +0800

    BuyAndSellActivity. Add. Butter knife

--- Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/BuyAndSellActivity.kt
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/BuyAndSellActivity.kt
@@ -1,13 +1,16 @@
 package com.jackpan.stockcomputer.Kotlin
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import butterknife.ButterKnife
+import com.jackpan.stockcomputer.Activity.BaseAppCompatActivity
 import com.jackpan.stockcomputer.R
 
-class BuyAndSellActivity : AppCompatActivity() {
+class BuyAndSellActivity : BaseAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_and_sell)
+        ButterKnife.bind(this)
+
     }
 }
