003cd5cbd5c89b3dba35daa1bc2a6071821be283
JackPan <jheng.wei0919@gmail.com>
2018-01-16 18:51:49 +0800

    Add BuyAndSellActivity It is write for kotlin

---
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/BuyAndSellActivity.kt
@@ -0,0 +1,13 @@
+package com.jackpan.stockcomputer.Kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jackpan.stockcomputer.R
+
+class BuyAndSellActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_and_sell)
+    }
+}
