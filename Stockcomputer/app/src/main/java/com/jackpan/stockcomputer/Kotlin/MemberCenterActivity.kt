a18cf2ee63550565b53df5f55a62ce3d323f292c
JackPan <jheng.wei0919@gmail.com>
2018-02-12 11:58:30 +0800

    Add member center  logout Add member user data

--- Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/MemberCenterActivity.kt
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/MemberCenterActivity.kt
@@ -7,6 +7,8 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import com.jackpan.stockcomputer.Activity.BaseAppCompatActivity
+import com.jackpan.stockcomputer.Data.MyApi
+import com.jackpan.stockcomputer.MySharedPrefernces
 import com.jackpan.stockcomputer.R
 import kotlinx.android.synthetic.main.activity_member_center.*
 
@@ -27,12 +29,43 @@ class MemberCenterActivity : BaseAppCompatActivity() {
         setSupportActionBar(toolbar)
         ButterKnife.bind(this)
         setTitle("會員中心")
+        getUserState()
 
 
+
+
+    }
+    fun getUserState() {
+        var userId: String = MySharedPrefernces.getUserId(this)
+        var userName: String = MySharedPrefernces.getUserName(this)
+        var userEmail: String = MySharedPrefernces.getUserMail(this)
+        var userPhoto: String = MySharedPrefernces.getUserPhoto(this)
+
+        if (!userId.equals("")){
+            mUserIdTextView.text = userId
+        }
+
+        if (!userName.equals("")){
+            mUserNameTextView.text = userName
+        }
+
+        if(!userEmail.equals("")){
+            mUserMailTextView.text = userEmail
+        }
+        if(!userPhoto.equals("")){
+            MyApi.loadImage(userPhoto,mUserImg,this)
+
+        }
     }
 
     @OnClick(R.id.userlogoutbtn)
     fun setUserLoginButton(){
+        MySharedPrefernces.saveUserId(this,"")
+        MySharedPrefernces.saveUserName(this,"")
+        MySharedPrefernces.saveUserMail(this,"")
+        MySharedPrefernces.saveUserPhoto(this,"")
+        finish()
+
 
     }
 
