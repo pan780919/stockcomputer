6ae39f0886fe789f72d5dc4e0299a9256024a332
JackPan <jheng.wei0919@gmail.com>
2018-02-22 16:07:31 +0800

    修正臉書登出登入流程

--- Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Manager/FacebookManager.java
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Manager/FacebookManager.java
@@ -18,7 +18,6 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -61,7 +60,7 @@ public class FacebookManager {
                         Log.d("LoginActivity", object.toString());
                         // Get facebook data from login
                         Bundle bFacebookData = getFacebookData(context,object);
-                        LoginManager.getInstance().logOut();
+//                        LoginManager.getInstance().logOut();
                         ((Activity)context).finish();
 
                     }
