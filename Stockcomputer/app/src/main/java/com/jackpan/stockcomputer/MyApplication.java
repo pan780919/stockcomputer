1b39fc67a9df83b2e16a7b7d3f0a1ce98bd73976
JackPan <JackPan@jackpan-MacBook-Pro.local>
2018-08-11 22:44:30 +0800

    change minSdkVersion to 15 and fix  application to multdexApplication add sha1 to firebase  console

--- Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/MyApplication.java
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/MyApplication.java
@@ -2,6 +2,7 @@ package com.jackpan.stockcomputer;
 
 import android.app.Application;
 import android.content.Intent;
+import android.support.multidex.MultiDexApplication;
 
 import com.adlocus.PushAd;
 import com.firebase.client.Firebase;
@@ -9,7 +10,7 @@ import com.jackpan.stockcomputer.Data.MyApi;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.FirebaseDatabase;
-public class MyApplication extends Application {
+public class MyApplication extends MultiDexApplication {
     private static MyApplication myApplication;
     public static MyApplication getInstance() {
         return myApplication;
