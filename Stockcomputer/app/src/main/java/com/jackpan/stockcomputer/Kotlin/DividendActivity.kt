07965d8739b6c4f3fc76b77a04f7f8221026c3e7
wei0820 <jheng.wei0919@gmail.com>
2018-08-27 16:02:22 +0800

    addd initlayout

--- Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/DividendActivity.kt
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/DividendActivity.kt
@@ -3,6 +3,8 @@ package com.jackpan.stockcomputer.Kotlin
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.jackpan.stockcomputer.R
@@ -11,12 +13,20 @@ import kotlinx.android.synthetic.main.activity_dividend.*
 
 class DividendActivity : AppCompatActivity() {
     lateinit var mAdView : AdView
-
+    lateinit var mInput_stock_price:EditText
+    lateinit var mInput_stock_now_price :EditText
+    lateinit var mInput_stock_amoumt :EditText
+    lateinit var mMoneybutton :Button
+    lateinit var mInput_money_now_price :EditText
+    lateinit var mInput_money_amoumt :EditText
+    lateinit var mStockbutton :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.content_dividend)
         title = "配股配息計算"
         setAdView()
+        initLayout()
+
 
     }
     fun  setAdView(){
@@ -24,5 +34,13 @@ class DividendActivity : AppCompatActivity() {
         var adRequset = AdRequest.Builder().build()
         mAdView.loadAd(adRequset)
     }
-
+    fun initLayout(){
+        mInput_stock_price = findViewById(R.id.input_stock_price)
+        mInput_stock_now_price = findViewById(R.id.input_stock_now_price)
+        mInput_stock_amoumt = findViewById(R.id.input_money_amoumt)
+        mInput_money_now_price = findViewById(R.id.input_money_price)
+        mInput_money_amoumt = findViewById(R.id.input_money_amoumt)
+        mMoneybutton = findViewById(R.id.moneybutton)
+        mStockbutton = findViewById(R.id.stockbutton)
+    }
 }
