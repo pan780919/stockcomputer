44369881815144275096eca23eb4b9a5ac361702
wei0820 <jheng.wei0919@gmail.com>
2018-12-27 10:43:04 +0800

    fix error

--- Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/util/StockPriceCheck.java
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/util/StockPriceCheck.java
@@ -6,12 +6,12 @@ import android.widget.TextView;
 public class StockPriceCheck {
 
     public static void  check(String s , TextView view){
-        String[] arrray = s.split("");
+        String[] arrray = s.split(" ");
         for (String s1 : arrray) {
-            if (s.contains("%")){
-                if (s.contains("-")){
+            if (s1.contains("%")){
+                if (s1.contains("-")){
                     view.setTextColor(Color.GREEN);
-                }else if(s.contains("+")){
+                }else if(s1.contains("+")){
                     view.setTextColor(Color.RED);
                 }
             }
