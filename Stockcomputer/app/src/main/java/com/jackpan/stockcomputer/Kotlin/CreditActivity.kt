b789bd3bb07ca370d5ef6cfa125b8f75d22c06d0
wei0820 <jheng.wei0919@gmail.com>
2018-12-27 14:00:25 +0800

    update vpon sdk

--- Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/CreditActivity.kt
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/CreditActivity.kt
@@ -3,8 +3,17 @@ package com.jackpan.stockcomputer.Kotlin
 import android.os.Bundle
 import com.jackpan.stockcomputer.Activity.BaseAppCompatActivity
 import com.jackpan.stockcomputer.R
-class CreditActivity : BaseAppCompatActivity() {
+import com.vpadn.ads.VpadnInReadAd
 
+class CreditActivity : BaseAppCompatActivity() {
+    // 宣告 VpadnInReadAd
+    private val inReadAd: VpadnInReadAd? = null
+
+    // 請將 License Key 換成 Vpon BD 提供您的 License Key
+    private val licenseKey = "License Key"
+
+    // 請替換成欲顯示廣告的位置
+    val AD_POSITION = 15
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credit)
