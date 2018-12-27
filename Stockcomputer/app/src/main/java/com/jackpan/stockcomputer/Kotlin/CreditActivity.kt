2d6d27ba5912255e47ba7ce651300212156e6f81
wei0820 <jheng.wei0919@gmail.com>
2018-12-27 13:48:37 +0800

    add CreditActivity

---
+++ Stockcomputer/app/src/main/java/com/jackpan/stockcomputer/Kotlin/CreditActivity.kt
@@ -0,0 +1,12 @@
+package com.jackpan.stockcomputer.Kotlin
+
+import android.os.Bundle
+import com.jackpan.stockcomputer.Activity.BaseAppCompatActivity
+import com.jackpan.stockcomputer.R
+class CreditActivity : BaseAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credit)
+    }
+}
